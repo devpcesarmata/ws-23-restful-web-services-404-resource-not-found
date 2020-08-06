c826e27585833b87ca9fa2f6fbe1da7054fe0915
César <dev.pcesarmata@gmail.com>
2020-08-05 20:59:36 -0500

    init commit

---
+++ src/main/java/com/cesarmata/ws23restfulwebservices404resourcenotfound/Ws23RestfulWebServices404ResourceNotFoundApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws23restfulwebservices404resourcenotfound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws23RestfulWebServices404ResourceNotFoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws23RestfulWebServices404ResourceNotFoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarmata/ws23restfulwebservices404resourcenotfound/Ws23RestfulWebServices404ResourceNotFoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws23restfulwebservices404resourcenotfound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws23RestfulWebServices404ResourceNotFoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
