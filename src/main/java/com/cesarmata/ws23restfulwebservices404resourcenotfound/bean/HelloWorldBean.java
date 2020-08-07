7236cd12ff1b8c63feb12bb9f18c1a7a7128b341
César <dev.pcesarmata@gmail.com>
2020-08-07 17:37:06 -0500

    Se termina con clase 24 del curso de udemy, se implementa respuesta 404 NOT FOUND

---
+++ src/main/java/com/cesarmata/ws23restfulwebservices404resourcenotfound/bean/HelloWorldBean.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.cesarmata.ws23restfulwebservices404resourcenotfound.bean;
+
+/**
+ * @author Cesar Mata
+ *
+ */
+public class HelloWorldBean {
+	private String message;
+	
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}	
+}
